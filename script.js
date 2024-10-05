4c2797ceab243d31b216bfa34b63ba6565a8c665
mcrow44 <143023921+mcrow44@users.noreply.github.com>
2024-10-05 00:47:33 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Our textarea
+const input = document.querySelector("#qrCodeTextArea");
+// Our canvas element with 'qr' id
+const canvas = document.getElementById("qr");
+
+// The various parameters
+const createQR = v => {
+  return new QRious({
+    element: canvas,
+    value: v,
+    level: "L",
+    size: 400,
+    backgroundAlpha: 0,
+    foreground: "white" });
+
+};
+
+// We create the qr code
+const qr = createQR(input.value);
+
+// If the text box changes, update the qr code.
+input.addEventListener("input", () => {
+  const qr = createQR(input.value);
+});
